3305cee6a031eb01a7880c53470cd16a3facf347
jayendrakasera <jayendrakasera05@gmail.com>
2026-06-19 13:32:33 +0530

    Initial Spring Boot project backend setup

---
+++ invoicekit/src/main/java/com/invoicekit/InvoicekitApplication.java
@@ -0,0 +1,13 @@
+package com.invoicekit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicekitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicekitApplication.class, args);
+	}
+
+}
---
+++ invoicekit/src/test/java/com/invoicekit/InvoicekitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoicekit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicekitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
